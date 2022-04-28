15779980b8a99b97f6c83422dd85e061815fdfa7
玖叁 <colour_93@furry.top>
2022-04-28 13:58:02 +0800

    彩蛋代码位置迁移以及格式化

--- docs/index.js
+++ docs/index.js
@@ -5,11 +5,11 @@ window.onload = () => {
     const text = document.querySelector('.bio-text');
 
     // 文本
-    const contentArray  = ["中国小动物开发社群", "Furry Developers in China"];
+    const contentArray = ["中国小动物开发社群", "Furry Developers in China"];
     let index = 0;
 
     // 颜文字
-    const emotionArray = ["(*/ω＼*)", "( •̀ ω •́ )y", "( •̀ .̫ •́ )✧","ヽ(＊>∇<)ﾉ","ฅ"];
+    const emotionArray = ["(*/ω＼*)", "( •̀ ω •́ )y", "( •̀ .̫ •́ )✧", "ヽ(＊>∇<)ﾉ", "ฅ"];
     let eIndex = 0;
 
     // 变向
@@ -19,7 +19,7 @@ window.onload = () => {
     let i = 0;
     let tempStr = contentArray[0] + "  " + emotionArray[0];
 
-    setInterval(()=>{
+    setInterval(() => {
 
         if (change) {
             text.innerHTML = tempStr.slice(0, ++i);
@@ -34,16 +34,16 @@ window.onload = () => {
 
         if (i == tempStr.length + 10) {
             change = false;
-        } else if ( i < 0 ) {   
+        } else if (i < 0) {
             i = 0;
             change = true;
             index++;
-            if ( index >= contentArray.length ) { 
+            if (index >= contentArray.length) {
                 text.innerHTML = "_";
                 index = 0;
             }
             eIndex++;
-            if ( eIndex >= emotionArray.length ) {
+            if (eIndex >= emotionArray.length) {
                 eIndex = 0;
             }
             tempStr = contentArray[index] + "  " + emotionArray[eIndex];
@@ -51,3 +51,34 @@ window.onload = () => {
 
     }, 100)
 }
+
+
+var styleTitle1 = `
+font-size: 20px;
+font-weight: 600;
+color: rgb(244,167,89);
+`
+var styleTitle2 = `
+font-style: oblique;
+font-size:14px;
+color: rgb(244,167,89);
+font-weight: 400;
+`
+var styleContent = `
+color: rgb(30,152,255);
+`
+
+/* 内容代码 */
+var title1 = '🌈 FurDevsCN '
+var title2 = '中国小动物开发社群'
+
+// => 读取配置型（在配置文件里配置这些会变动的网址）
+var offiUrl = 'https://furdevs.cn'
+var content = `
+版 本 号：1.0.0    
+欸？你发现了一个彩蛋？
+
+🏠官网:  ${offiUrl}
+`
+console.log(`%c${title1} %c${title2}
+%c${content}`, styleTitle1, styleTitle2, styleContent)
